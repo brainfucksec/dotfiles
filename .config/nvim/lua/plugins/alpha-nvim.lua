cdce183c30715b2918cfbdf90df1343b5fd2faae
brainfucksec <brainfucksec@protonmail.com>
2025-07-13 16:59:26 +0200

    polybar: Update v0.41.0 - Remove modules.ini - Fix some code
    Update nvim folder

--- .config/nvim/lua/plugins/alpha-nvim.lua
+++ .config/nvim/lua/plugins/alpha-nvim.lua
@@ -3,29 +3,28 @@
 -----------------------------------------------------------
 
 -- Plugin: alpha-nvim
--- url: https://github.com/goolord/alpha-nvim
+-- URL: https://github.com/goolord/alpha-nvim
 
--- For configuration examples see: https://github.com/goolord/alpha-nvim/discussions/16
-
+--[[
+Configuration start: https://github.com/goolord/alpha-nvim#quick-start
+Configuration examples (alpha-themes): https://github.com/goolord/alpha-nvim/discussions/16
+--]]
 
 local status_ok, alpha = pcall(require, 'alpha')
 if not status_ok then
   return
 end
 
+-- Use `dashboard-nvim` theme, other themes: `vim-startify` and `theta`
+-- see: https://github.com/goolord/alpha-nvim?tab=readme-ov-file#quick-start
 local dashboard = require('alpha.themes.dashboard')
 
--- Footer
-local function footer()
-  local version = vim.version()
-  local print_version = "v" .. version.major .. '.' .. version.minor .. '.' .. version.patch
-  local datetime = os.date('%Y/%m/%d %H:%M:%S')
 
-  return print_version .. ' - ' .. datetime
-end
-
--- Banner
-local banner = {
+-- Load banner:
+-- Set the different banners to the variables `bx` (b0,b1,..),
+-- to add a new banner create a new variable `bx` to the list :)
+-- By default there are six different banners (from b0 to b5).
+local b0 = {
   "                                                    ",
   " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
   " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
@@ -36,16 +35,89 @@ local banner = {
   "                                                    ",
 }
 
-dashboard.section.header.val = banner
+local b1 = {
+  "                                                   ",
+  " ███    ██ ███████  ██████  ██    ██ ██ ███    ███ ",
+  " ████   ██ ██      ██    ██ ██    ██ ██ ████  ████ ",
+  " ██ ██  ██ █████   ██    ██ ██    ██ ██ ██ ████ ██ ",
+  " ██  ██ ██ ██      ██    ██  ██  ██  ██ ██  ██  ██ ",
+  " ██   ████ ███████  ██████    ████   ██ ██      ██ ",
+  "                                                   "
+}
 
--- Menu
+local b2 = {
+  "                                                              ",
+  " ░   ░░░  ░░        ░░░      ░░░  ░░░░  ░░        ░░  ░░░░  ░ ",
+  " ▒    ▒▒  ▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒▒▒▒  ▒▒▒▒▒   ▒▒   ▒ ",
+  " ▓  ▓  ▓  ▓▓      ▓▓▓▓  ▓▓▓▓  ▓▓▓  ▓▓  ▓▓▓▓▓▓  ▓▓▓▓▓        ▓ ",
+  " █  ██    ██  ████████  ████  ████    ███████  █████  █  █  █ ",
+  " █  ███   ██        ███      ██████  █████        ██  ████  █ ",
+  "                                                              "
+}
+
+local b3 = {
+  "                                                     ",
+  "  ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓  ",
+  "  ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒  ",
+  " ▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░ ",
+  " ▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██  ",
+  " ▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒ ",
+  " ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░ ",
+  " ░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░ ",
+  "    ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░    ",
+  "          ░    ░  ░    ░ ░        ░   ░         ░    ",
+  "                                 ░                   ",
+  "                                                     "
+}
+
+local b4 = {
+  "                                                      ",
+  " ##    ## ########  #######  ##     ## #### ##     ## ",
+  " ###   ## ##       ##     ## ##     ##  ##  ###   ### ",
+  " ####  ## ##       ##     ## ##     ##  ##  #### #### ",
+  " ## ## ## ######   ##     ## ##     ##  ##  ## ### ## ",
+  " ##  #### ##       ##     ##  ##   ##   ##  ##     ## ",
+  " ##   ### ##       ##     ##   ## ##    ##  ##     ## ",
+  " ##    ## ########  #######     ###    #### ##     ## ",
+  "                                                      "
+}
+
+local b5 = {
+  "                                                             ",
+  " '##::: ##:'########::'#######::'##::::'##:'####:'##::::'##: ",
+  "  ###:: ##: ##.....::'##.... ##: ##:::: ##:. ##:: ###::'###: ",
+  "  ####: ##: ##::::::: ##:::: ##: ##:::: ##:: ##:: ####'####: ",
+  "  ## ## ##: ######::: ##:::: ##: ##:::: ##:: ##:: ## ### ##: ",
+  "  ##. ####: ##...:::: ##:::: ##:. ##:: ##::: ##:: ##. #: ##: ",
+  "  ##:. ###: ##::::::: ##:::: ##::. ## ##:::: ##:: ##:.:: ##: ",
+  "  ##::. ##: ########:. #######::::. ###::::'####: ##:::: ##: ",
+  " ..::::..::........:::.......::::::...:::::....::..:::::..:: ",
+  "                                                             "
+}
+
+-- Assigns the value of the preferred `bx` banner to `section.header.val`
+dashboard.section.header.val = b1
+
+-- Menu (Shortcuts)
 dashboard.section.buttons.val = {
   dashboard.button('e', '  New file', ':ene <BAR> startinsert<CR>'),
-  dashboard.button('f', '  Find file', ':NvimTreeOpen<CR>'),
+  dashboard.button('f', '  Find file', ':NvimTreeOpen<CR>'),
   dashboard.button('s', '  Settings', ':e $MYVIMRC<CR>'),
   dashboard.button('u', '  Update plugins', ':Lazy update<CR>'),
   dashboard.button('q', '  Quit', ':qa<CR>'),
 }
+
+-- Load Footer
+local function footer()
+  -- Print nvim package version
+  local version = vim.version()
+  local print_version = "v" .. version.major .. '.' .. version.minor .. '.' .. version.patch
+  -- Print Datetime
+  local datetime = os.date('%Y/%m/%d %H:%M')
+
+  -- Example output: v0.10.4 - 2025/03/09 08:47
+  return print_version .. ' - ' .. datetime
+end
 
 dashboard.section.footer.val = footer()
 
